02209d87db1c40bb56d9927d8dbedf4242d96287
Sam Skjonsberg <skoneberg@gmail.com>
2013-07-24 21:52:02 -0700

    update mock data.

--- MockData.js
+++ MockData.js
@@ -4,72 +4,132 @@ define(function(require) {
         questions : [
             {
                 id : 1,
-                title : "What's a good place to eat?",
-                responseCount : 3,
-                body : "I'm in Portland for the weekend and am looking for a good place to get food.  Any suggestions?",
+                title : "Where do the locals eat?",
+                responseCount : 4,
+                body : "In the mood for something mid priced.",
                 user : {
                     id : 1,
                     imageUrl : "http://yabbly.local:4000/image/profile/1?s=100"
                 },
                 responses : [
                     {
-                        body : 'Try voodoo doughnuts!!!',
+                        body : 'Oooh, you must try Bizaro Cafe.  Ask for the best seat in the house and get the Putanesca.',
                         helpfulCount : 8,
                         user : {
-                            name : 'codeviking',
-                            id : 133,
-                            imageUrl : "http://yabbly.local:4000/image/profile/133?s=100"
+                            name : 'codeviking'
+                        }
+                    },
+                    {
+                        body : 'There\'s a whole lotta Thai options.  If you can find parking, try Thai Tom.  It\'s a few minutes from you.',
+                        helpfulCount : 3,
+                        user : {
+                            name : 'tom'
+                        }
+                    },
+                    {
+                        body : 'Stop the car!  You\'re right next to my favorite ramen place (Aloha Ramen).',
+                        helpfulCount : 1,
+                        user : {
+                            name : 'ian'
+                        }
+                    },
+                    {
+                        body : 'It\'s a 15 minute drive but I\'d head south to Matt\'s in the Market.  Based on your Tens list, I think you\'ll like it.',
+                        helpfulCount : 2,
+                        user : {
+                            name : 'khaled'
                         }
                     }
                 ]
             },
             {
                 id : 2,
-                title : "Where am I going?",
-                responseCount : 5,
-                body : "I have no idea where I'm driving",
+                title : "Best place for a family dinner?",
+                responseCount : 3,
+                body : "Kids say they want italian.",
                 user : {
                     id : 1,
                     imageUrl : "http://yabbly.local:4000/image/profile/1?s=100"
                 },
                 responses : [
                     {
-                        helpfulCount : 4,
-                        body : 'No where!',
+                        helpfulCount : 1,
+                        body : 'Ridge Pizza has big tables and is choc full of kids.  Decent spaghetti and meatballs too!',
                         user : {
-                            id : 133,
-                            name : 'codeviking',
-                            imageUrl : "http://yabbly.local:4000/image/profile/133?s=100"
+                            name : 'steven',
                         }
                     },
                     {
-                        helpfulCount : 1,
-                        body : 'Somewhere!',
+                        helpfulCount : 2,
+                        body : 'There isn\tt great Italian where you are but if they want Pizza, I\'d go to Veraci and get some to go then eat it by Green Lake.',
                         user : {
-                            id : 2133,
-                            name : 'tom',
-                            imageUrl : "http://yabbly.local:4000/image/profile/2?s=100"
+                            name : 'tom'
                         }
                     },
+                    {
+                        helpfulCount : 4,
+                        body : 'If the kids aren\'t too small, Brad\'s Swingside is surprisingly good but it\'s a pretty cozy place so not for toddlers!',
+                        user : {
+                            name : 'tedb'
+                        }
+                    }
                 ]
             },
             {
                 id : 3,
-                title : "What's a good hotel nearby?",
-                body : "I need a place to stay!",
-                responseCount : 13,
+                title : "Best place for killer shopping?",
+                body : "Wife is looking for new shoes.",
+                responseCount : 3,
                 user : {
                     id : 1,
                     imageUrl : "http://yabbly.local:4000/image/profile/1?s=100"
                 },
                 responses : [
                     {
-                        helpfulCount : 3,
-                        body : 'The Marriot!',
+                        helpfulCount : 0,
+                        body : 'You\'ve got two options: Pacific Place or Bellevue Square.',
                         user : {
-                            id : 133,
                             name : 'codeviking',
-                            imageUrl : "http://yabbly.local:4000/image/profile/133?s=100"
+                        }
+                    },
+                    {
+                        helpfulCount : 2,
+                        body : 'You could try U Village but you\'re not far from Nordstrom flagship store.',
+                        user : {
+                            name : 'iamjane',
+                        }
+                    },
+                    {
+                        helpfulCount : 0,
+                        body : 'SPONSORED: Market Street shoes is offering a 10% off coupon if you come in before 5PM.',
+                        user : {
+                            name : 'marketshoes',
+                        }
+                    }
+                ]
+            },
+            {
+                id : 4,
+                title : "Best place for live entertainment?",
+                body : "Looking for something chillaxing.",
+                responseCount : 2,
+                user : {
+                    id : 1,
+                    imageUrl : "http://yabbly.local:4000/image/profile/1?s=100"
+                },
+                responses : [
+                    {
+                        helpfulCount : 1,
+                        body : 'Triple Door is good.  Feel free to call me and I can look up what\'s showing if you\'re driving.',
+                        user : {
+                            name : 'tom',
+                        }
+                    },
+                    {
+                        helpfulCount : 0,
+                        body : 'I\'m traveling there next week, can you update this thread wherever u decide?',
+                        user : {
+                            name : 'flyfrank',
                         }
                     }
                 ]
